fad7a9d34682117ed6b47d3f07a9b7ea29e20b43
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-08-09 16:46:16 -0400

    Basic GET request handling

--- server.js
+++ server.js
@@ -1,34 +1,21 @@
 const fs = require("fs");
 const { createServer } = require("node:http");
+const sqlite3 = require("sqlite3").verbose();
+
+const db = new sqlite3.Database(":memory:");
 
 const port = 3000;
 const hostname = "127.0.0.1";
 
-function jsTimeFormat(unix) {
-    return `<script>var date = new Date(${unix}); document.write(date.toLocaleDateString() + " " + date.toLocaleTimeString());</script>`;
-}
+// function jsTimeFormat(unix) {
+//     return `<script>var date = new Date(${ unix }); document.write(date.toLocaleDateString() + " " + date.toLocaleTimeString());</script>`;
+// }
 
-function respondIndex(res, error = "") {
+// function respond400(req, res) {
 
-    var indexText = fs.readFileSync("html/index.html", "utf8");
-
-    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-    res.end(indexText);
-}
-
-function respondImage(res, path) {
-
-    var image = fs.readFileSync("." + path);
-
-    res.writeHead(200, { "Content-Type": "image/" + path.split(".").at(-1) });
-    res.end(image);
-}
-
-function respond400(req, res) {
-
-    res.writeHead(400, { "Content-Type": "text/plain" });
-    res.end("Error 400: Bad request endpoint\n" + req.method + " " + req.url);
-}
+//     res.writeHead(400, { "Content-Type": "text/plain" });
+//     res.end("Error 400: Bad request endpoint\n" + req.method + " " + req.url);
+// }
 
 function post(req, maxBytes, onExcessivelyHeavy, onSuccessfulRead) {
 
@@ -39,7 +26,7 @@ function post(req, maxBytes, onExcessivelyHeavy, onSuccessfulRead) {
 
         body += data;
 
-        // Too much POST data, kill the connection!
+        // too much POST data, kill the connection!
         if (!didRespond && body.length > maxBytes) {
             onExcessivelyHeavy();
             didRespond = true;
@@ -55,72 +42,97 @@ function post(req, maxBytes, onExcessivelyHeavy, onSuccessfulRead) {
     });
 }
 
-function postToIndex(req, res) {
+// function postToIndex(req, res) {
 
-    post(
-        req,
-        1e6, // 1mb
-        function () {
-            respondIndex(res, "Post data too heavy! Try again with fewer bytes!");
-        },
-        function (post) {
+//     post(
+//         req,
+//         1e6, // 1mb
+//         function () {
+//             respondIndex(res, "Post data too heavy! Try again with fewer bytes!");
+//         },
+//         function (post) {
 
-            // validate
-            if (post.title == undefined || post.title.trim().length == 0) {
-                respondIndex(res, "Please enter a title!");
-                return;
-            }
+//             // validate
+//             if (post.title == undefined || post.title.trim().length == 0) {
+//                 respondIndex(res, "Please enter a title!");
+//                 return;
+//             }
 
-            if (post.title.trim().length > 80) {
-                respondIndex(res, "Title must be >=80 characters!");
-                return;
-            }
+//             if (post.title.trim().length > 80) {
+//                 respondIndex(res, "Title must be >=80 characters!");
+//                 return;
+//             }
 
-            if (post.message == undefined || post.message.trim().length == 0) {
-                respondIndex(res, "Please enter a message!");
-                return;
-            }
+//             if (post.message == undefined || post.message.trim().length == 0) {
+//                 respondIndex(res, "Please enter a message!");
+//                 return;
+//             }
 
-            // create thread with unique id
-            post.title = post.title.trim();
-            post.message = post.message.trim();
+//             // create thread with unique id
+//             post.title = post.title.trim();
+//             post.message = post.message.trim();
             
-            var threadID = 0;
+//             var threadID = 0;
 
-            while (db.threads[threadID] != undefined) {
-                threadID = Math.floor(Math.random() * 1000);
-            }
+//             while (db.threads[threadID] != undefined) {
+//                 threadID = Math.floor(Math.random() * 1000);
+//             }
 
-            db.threads[threadID] = {
-                "title": post.title,
-                "posts": [
-                    {
-                        "message": post.message,
-                        "unixtime": Date.now()
-                    }
-                ]
-            };
+//             db.threads[threadID] = {
+//                 "title": post.title,
+//                 "posts": [
+//                     {
+//                         "message": post.message,
+//                         "unixtime": Date.now()
+//                     }
+//                 ]
+//             };
 
-            respondIndex(res);
+//             respondIndex(res);
+//         }
+//     );
+// }
+
+const endpoints = [
+    {
+        regex: new RegExp("^GET /$"),
+        respond: (req, res) => {
+
+            const indexText = fs.readFileSync("index.html", "utf8");
+
+            res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+            res.end(indexText);
         }
-    );
-}
+    },
+    {
+        regex: new RegExp("^GET /img/"),
+        respond: (req, res) => {
 
-const server = createServer((req, res) => {
+            const image = fs.readFileSync("." + req.url);
 
-    const endpoint = req.method + " " + req.url;
-    console.log(endpoint);
-
-    try {
-
-        // match endpoints
-
-    } catch (err) {
-
-        // something went wrong, and it's not the client's fault this time!
-        res.writeHead(500, { "Content-Type": "text/plain" });
-        res.end("500 Internal Server Error\n" + err);
+            res.writeHead(200, { "Content-Type": "image/" + req.url.split(".").at(-1) });
+            res.end(image);
+        }
     }
-});
+];
 
-server.listen(port, hostname, () => console.log(`Starting @ http://${ hostname }:${ port }/`));
+createServer((req, res) => {
+
+    const requested_endpoint = req.method + " " + req.url;
+    console.log(requested_endpoint);
+
+    // match endpoints
+    for (const endpoint of endpoints) {
+
+        if (endpoint.regex.test(requested_endpoint)) {
+
+            endpoint.respond(req, res);
+            return;
+        }
+    }
+
+    // return 404 if no endpoint matched
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("404 Not Found");
+
+}).listen(port, hostname, () => console.log(`Starting @ http://${ hostname }:${ port }/`));
