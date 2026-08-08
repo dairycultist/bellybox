f296c04611039c94286608692cf3568b99b31c36
dairycultist <kazumafan46@gmail.com>
2026-08-08 17:23:45 +0200

    Filtering by tags is back and cooperating with pagination

--- endpoints/index.js
+++ endpoints/index.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-const itemsPerPage = 10;
+const itemsPerPage = 2;
 
 module.exports = [
     // index (potentially including tag search and page)
@@ -8,7 +8,21 @@ module.exports = [
         regex: new RegExp("^GET /(\\\?.*)?$"),
         respond: (respondImagePage, getSPA, db, query, req, res) => {
 
-            db.get(`SELECT COUNT(*) AS count FROM Images`, (err, count) => {
+            const tags = query.getAll("tag");
+
+            const tagWhere = tags.length == 0 ? "" : " WHERE " + tags.map(tag => `Tags LIKE "%${ tag }%"`).join(" AND ");
+
+            const tagQuery = (() => {
+
+                let construct = "";
+
+                for (const tag of tags)
+                    construct += "&tag=" + tag;
+
+                return construct;
+            })();
+
+            db.get(`SELECT COUNT(*) AS count FROM Images${ tagWhere }`, (err, count) => {
 
                 count = count.count;
 
@@ -26,7 +40,7 @@ module.exports = [
 
                 let images = "<div style='display: flex; flex-wrap: wrap;'>";
 
-                db.each(`SELECT ID, Filename FROM Images LIMIT ${ itemsPerPage } OFFSET ${ (pageIndex - 1) * itemsPerPage };`, (err, row) => {
+                db.each(`SELECT ID, Filename FROM Images${ tagWhere } LIMIT ${ itemsPerPage } OFFSET ${ (pageIndex - 1) * itemsPerPage };`, (err, row) => {
 
                     images += `<a href="image/${ row.ID }" class="galleryitem"><img src="/img/${ row.Filename }"></a>`;
 
@@ -39,36 +53,11 @@ module.exports = [
                         .replaceAll("<!-- total result items -->", count)
                         .replaceAll("<!-- page index -->", pageIndex)
                         .replaceAll("<!-- page total -->", pageTotal)
-                        .replaceAll("PREV_PAGE_HREF", pageIndex == 1         ? "" : `href="?page=${ pageIndex - 1 }"`) // remember filter
-                        .replaceAll("NEXT_PAGE_HREF", pageIndex == pageTotal ? "" : `href="?page=${ pageIndex + 1 }"`)
+                        .replaceAll("PREV_PAGE_HREF", pageIndex == 1         ? "" : `href="?page=${ pageIndex - 1 }${ tagQuery }"`)
+                        .replaceAll("NEXT_PAGE_HREF", pageIndex == pageTotal ? "" : `href="?page=${ pageIndex + 1 }${ tagQuery }"`)
                     );
                 });
             });
         }
-    },
-    // // index + tag search
-    // {
-    //     regex: new RegExp("^GET /\\\?tag="),
-    //     respond: (respondImagePage, getSPA, db, req, res) => {
-
-    //         const tagWhere = req.url.split("?tag=", 2)[1].split("&tag=").map(tag => `Tags LIKE "%${ tag }%"`).join(" AND ");
-
-    //         let images = "<div style='display: flex; flex-wrap: wrap;'>";
-
-    //         db.each(`SELECT ID, Filename, MasonryFlex FROM Images WHERE ${ tagWhere };`, (err, row) => {
-
-    //             images += `<a href="image/${ row.ID }" style="flex: ${ row.MasonryFlex } 1 ${ row.MasonryFlex }px; width: ${ row.MasonryFlex }px;"><img src="/img/${ row.Filename }" style="width: 100%;"></a>`;
-
-    //         }, () => {
-
-    //             // adding this at the end prevents the last row from filling the whole row
-    //             images += "<div style='flex: 200000;'></div>";
-    //             images += "</div>";
-                
-    //             // respond on complete
-    //             res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-    //             res.end(getSPA(images));
-    //         });
-    //     }
-    // }
+    }
 ];
